3faaee82643891137d409a008854ddf88b631626
jhwebtech <julieh71@ymail.com>
2016-03-31 14:14:43 -0500

    updated server file

--- server.js
+++ server.js
@@ -4,14 +4,11 @@ var PORT = 3000;
 
 var middleware = require('./middleware.js')
 
-
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function(req, res){
 	res.send('About Us :)');
 });
-
-
 
 app.use(express.static(__dirname + '/public'));
 
